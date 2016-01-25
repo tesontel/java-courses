de0d659402741a565c1cf84f0a2d86a199ef18e7
Василий Филимонов < >
2016-01-26 01:56:46 +0300

    creat Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,20 @@
 public class Calculate {
 
 	public static void main (String[] arg) {
-		System.out.println("Calculate...");	
+		System.out.println("Calculate...");
+		Double first = Double.valueOf(arg[0]);
+		Double second = Double.valueOf(arg[1]);
+		Double summ = first + second;
+		Double subtraction = first - second;
+		Double multiplication = first * second;
+		Double division = first / second;
+		
+		System.out.println("Sum..." +summ);
+		System.out.println("Subtraction..." +subtraction);	
+		System.out.println("Multiplication..." +multiplication);
+		System.out.println("Division..." +division);
+	
+		
+	
 	}
 }
